2cccb99ecb6a2bc287da552d48560fc132d0b30a
Dennis Castro <denniscas2000@gmail.com>
2023-12-27 19:13:44 -0600

    add intro

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.logo-new');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    setTimeout(()=>{
+        logoSpan.forEach((span, idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active')
+            }, (idx + 1) * 400)
+        })
+        setTimeout(()=>{
+            logoSpan.forEach((span, idx)=>{
+                setTimeout(()=>{
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                }, (idx + 1)* 50)
+            })
+        },2000);
+
+        setTimeout(()=>{
+            intro.style.top = '-100vh';
+        }, 2300)
+    })
+})
